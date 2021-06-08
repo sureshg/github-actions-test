d11ac50acb1e90ec758bafed85d0191059afb9b0
Suresh <sureshg@users.noreply.github.com>
2021-06-08 00:12:53 -0700

    Update the java application

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 import static java.lang.System.out;
 
+import java.io.File;
 import java.net.InetAddress;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyStore;
+import java.time.ZoneId;
 import java.util.Arrays;
-import java.util.TimeZone;
-import java.util.stream.Collectors;
+import java.util.HexFormat;
+import java.util.Locale;
 import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
@@ -13,41 +16,68 @@ public class Main {
 
   public static void main(String[] args) throws Exception {
 
-    out.println("<----- Processes ----->");
+    final var lineSep =  System.lineSeparator();
+
+    out.printf("%n✧✧✧✧✧ Processes ✧✧✧✧✧%n");
     var ps = ProcessHandle.allProcesses()
         .sorted(ProcessHandle::compareTo)
-        .collect(Collectors.toList());
+        .toList();
     ps.forEach(p -> {
       var pInfo = p.pid() + " : " + p.info();
       out.println(pInfo);
     });
 
-    out.println("\n<----- Trust stores ----->");
+    out.printf("%n✧✧✧✧✧ Trust stores ✧✧✧✧✧%n");
     var tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
     tmf.init((KeyStore) null);
     var issuers = Arrays.stream(tmf.getTrustManagers()).flatMap(tm -> {
       var x509Tm = (X509TrustManager) tm;
       return Arrays.stream(x509Tm.getAcceptedIssuers());
-    }).collect(Collectors.toList());
-    issuers.forEach(cert -> out.println(cert.getIssuerDN()));
+    }).toList();
+    issuers.forEach(cert -> out.println(cert.getIssuerX500Principal()));
 
-    out.println("\n<----- Dns Resolution ----->");
-    var dns = Arrays.stream(InetAddress.getAllByName("google.com")).collect(Collectors.toList());
+    out.printf("%n✧✧✧✧✧ Dns Resolution ✧✧✧✧✧%n");
+    var dns = Arrays.stream(InetAddress.getAllByName("google.com")).toList();
     dns.forEach(out::println);
 
-    out.println("\n<----- TimeZones ----->");
-    var tz = Arrays.stream(TimeZone.getAvailableIDs()).collect(Collectors.toList());
+    out.printf("%n✧✧✧✧✧ TimeZones ✧✧✧✧✧%n");
+    var tz = ZoneId.getAvailableZoneIds();
     tz.forEach(out::println);
 
-    out.println("\n<----- Charsets ----->");
+    out.printf("%n✧✧✧✧✧ Charsets ✧✧✧✧✧%n");
     var cs = Charset.availableCharsets();
     cs.forEach((name, charSet) -> out.println(name + " : " + charSet));
 
-    var stats = "\nProcesses      : " + ps.size() + "\n" +
-        "Dns Addresses  : " + dns.size() + "\n" +
-        "Trust Stores   : " + issuers.size() + "\n" +
-        "TimeZones      : " + tz.size() + "\n" +
-        "CharSets       : " + ps.size() + "\n";
+    out.printf("%n✧✧✧✧✧ System Locales ✧✧✧✧✧%n");
+    var locales = Locale.getAvailableLocales();
+    for (Locale locale : locales) {
+      out.println(locale);
+    }
+
+    out.printf("%n✧✧✧✧✧ Env Variables ✧✧✧✧✧%n");
+    var env =  System.getenv();
+    env.forEach((k, v) -> out.println(k + " : " + v));
+
+    out.printf("%n✧✧✧✧✧ System Properties ✧✧✧✧✧%n");
+    var props = System.getProperties();
+    props.forEach((k, v) -> out.println(k + " : " + v));
+
+    var lineSepHex = HexFormat.of().formatHex(lineSep.getBytes(StandardCharsets.UTF_8));
+    out.printf("%n✧✧✧✧✧ LineSeparator = %s%n", lineSepHex);
+    out.printf("%n✧✧✧✧✧ File PathSeparator = %s%n", File.pathSeparator);
+
+    var stats = """
+        +------------------------+
+        | Processes      : %5d |
+        | Dns Addresses  : %5d |
+        | Trust Stores   : %5d |
+        | TimeZones      : %5d |
+        | CharSets       : %5d |
+        | Locales        : %5d |
+        | Env Vars       : %5d |
+        | Sys Props      : %5d |
+        +------------------------+
+        """.formatted(ps.size(), dns.size(), issuers.size(), tz.size(), cs.size(),locales.length, env.size(),props.size());
 
     out.println(stats);
   }
