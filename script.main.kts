8636ec4b0aa03ca7164633f390287ebdfc180471
Suresh G <sureshg.g@gmail.com>
2022-11-08 18:44:19 -0800

    chore: github action changes

--- script.main.kts
+++ script.main.kts
@@ -1,14 +1,9 @@
-#!/usr/bin/env -S kotlin -Xplugin=/usr/local/opt/kotlin/libexec/lib/kotlinx-serialization-compiler-plugin.jar
+#!/usr/bin/env -S kotlin -Xplugin=/opt/homebrew/opt/kotlin/libexec/lib/kotlinx-serialization-compiler-plugin.jar
 
 // @file:Repository("https://maven.google.com")
-@file:DependsOn("io.ktor:ktor-client-core:2.0.0")
-@file:DependsOn("io.ktor:ktor-client-cio:2.0.0")
-@file:DependsOn("io.ktor:ktor-client-java:2.0.0")
-@file:DependsOn("io.ktor:ktor-client-auth:2.0.0")
-@file:DependsOn("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-@file:DependsOn("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
-@file:DependsOn("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
-@file:DependsOn("org.slf4j:slf4j-simple:2.0.0-alpha7")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+@file:DependsOn("org.slf4j:slf4j-simple:2.0.3")
 
 @file:CompilerOptions("-opt-in=kotlin.RequiresOptIn", "-jvm-target", "1.8")
 @file:OptIn(ExperimentalStdlibApi::class, ExperimentalSerializationApi::class)
